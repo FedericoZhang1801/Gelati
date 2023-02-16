7540705e8711f37b4db53336f33eab1d0fe49077
Federico Zhang <116790986+FedericoZhang1801@users.noreply.github.com>
2023-02-16 10:31:11 +0100

    Add files via upload

---
+++ Zhang.WenXiangFederico.4h.gelati/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Zhang.WenXiangFederico._4h.gelati.Models;
+
+namespace Zhang.WenXiangFederico._4h.gelati
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+                Gelati gg = new Gelati("db.csv");
+                dgDati.ItemsSource = gg;    
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ Zhang.WenXiangFederico.4h.gelati/Models/Gelato.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhang.WenXiangFederico._4h.gelati.Models
+{
+    public class Gelato
+    {
+        public string Tipo { get; set; }
+        public string Size { get; set; }
+        public string Gusto { get; set; }
+        public Gelato() { }
+
+        public Gelato(string riga)
+        {
+            string[] colonne = riga.Split(';');
+            Tipo = colonne[0];
+            Size = colonne[1];
+            Gusto = colonne[2];
+        }
+
+    }
+
+    public class Gelati:List<Gelato>
+    {
+        public Gelati() { }
+
+        public Gelati(string nomefile)
+        {
+            try
+            {
+
+                StreamReader fin = new StreamReader(nomefile);
+                fin.ReadLine();
+                while(!fin.EndOfStream)
+                {
+                    string riga = fin.ReadLine();
+                    Gelato g = new Gelato(riga);
+                    Add(g);
+                }
+                fin.Close();
+            }
+            catch(Exception ex) { }
+        }
+    }
+
+
+}
